95e592b53f11b5b027444d23b3e73f2189bd1e1b
Tarek Bouzo <tarekbozo@gmail.com>
2017-05-11 11:30:22 +0200

    Vehicle calss and the sub class

---
+++ MATJParking/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MATJParking
+{
+   abstract class Vehicle
+    {
+        public DateTime CheckInTime { get; set; }
+        public DateTime CheckOutTime { get; set; }
+        public string RegNumber { get; set; }
+        public double Price { get; }
+
+        //methods
+
+        protected abstract double GetPrice();
+       
+       
+    }
+}

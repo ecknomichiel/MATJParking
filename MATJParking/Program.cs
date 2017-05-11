73edbe9f0ef9e590f8e514490dd401d710c3a141
Tarek Bouzo <tarekbozo@gmail.com>
2017-05-11 16:00:46 +0200

    Implementing Checkin

--- MATJParking/Program.cs
+++ MATJParking/Program.cs
@@ -22,14 +22,43 @@ namespace MATJParking
                     +"\n3.Search for your Vehicle \n4.Search for multiple Vehicles \n5.See all Vehicles at Garage \n6.Exit");
       		    Console.WriteLine("Please enter your selection: ");
 
-                switch (Console.ReadLine())  //switch statement 
+                string input = Console.ReadLine();
+
+                switch (input)  //switch statement 
                 {
                     case "1":
+                        Console.WriteLine();
+                        Console.WriteLine("Add a new vehicle to the grage");
+                        Console.WriteLine("---------------------------------------");
+                       Console.WriteLine("Please Add the Registration Number");
+                       string regNumber =Console.ReadLine();
+                        Console.WriteLine("---------------------------------------");
+                        Console.WriteLine("Please chose what vehicle Type");
+                        Console.WriteLine("----------------------------------------");
+                        
+                        string vType = Console.ReadLine();
 
-                        case
+             
+                        garage.CheckIn(regNumber, vType);
+                        break;
 
+                    case "2":
+
+                        break;
+                    case "3":
+                        
                     break;
+                    case "4":
+
+                        break;
+                    case "5":
+
+                        break;
+
+                    default:
+                        break;
                 }
+
             }
         }
     }

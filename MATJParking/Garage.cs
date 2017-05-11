550ae0fe60ae026b8ec2760b71354cc925df10d2
AhAh10 <ahmadi2001@yahoo.com>
2017-05-11 11:33:04 +0200

    Add Parkingplaces and Garage

---
+++ MATJParking/Garage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MATJParking
+{
+    class Garage
+    {
+        private List<ParkingPlace> parkingplaces = new List<ParkingPlace>();
+      
+        public string CheckIn(string RegistrationNumber, string VehicleType)
+        {
+
+        }
+
+
+    }
+}
---
+++ MATJParking/ParkingPlace.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MATJParking
+{
+    class ParkingPlace
+    {
+        private object vehicle;
+        public Type vehicleType { get; set; }
+        public string ID { get; set; }
+        public bool Occupied { get {return vehicle != null;} }
+
+        public void Park(object Avehicle)
+        {
+            vehicle = Avehicle;
+           // Vehicle.CheckInTime = Datetime.Now;
+        }
+        public void Unpark()
+        {
+            //Vehicle.CheckOutTime = Datetime.Now;
+            vehicle = null;
+
+        }
+    }
+
+}

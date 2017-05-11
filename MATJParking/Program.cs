5a8032e18bc0e330d97e730986bcaa778f2c25ba
ecknomichiel <michiel@frostvikenit.eu>
2017-05-11 15:55:31 +0200

    Implemented 2 and 6 in main menu

--- MATJParking/Program.cs
+++ MATJParking/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MATJParking
 {
@@ -13,7 +10,7 @@ namespace MATJParking
         {
             //Main menu
             bool runProgram = true;
-            while (runProgram == true)
+            while (runProgram)
 
             {
                 Console.Clear();  // Clears console
@@ -24,10 +21,65 @@ namespace MATJParking
                 switch (Console.ReadLine())  //switch statement 
                 {
                     case "1":
+                        break;
+                    case "2":
+                        CheckOut();
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        break;
+                    case "6":
+                        runProgram = false;
+                        break;
+                }
+            }
+        }
 
-                     
+        private static void CheckOut()
+        {
+            Console.WriteLine("Check out\nWhat is the registration number of the car you want to check out?");
+            string regNr = Console.ReadLine();
+            try
+            {
+                ParkingPlace place = garage.SearchPlaceWhereVehicleIsParked(regNr);
+                if (place == null)
+                {
+                    Console.WriteLine("Cannot find the vehicle with registration numner '{0}'", regNr);
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("The vehicle is parked at {0}. Price is SEK {1}\nDo you want to checkout y/n?", place.ID, place.Vehicle.Price);
+                    switch (ConstrainInput("Do you want to checkout y/n?", new string[] { "y", "n" }))
+                    {
+                        case "y":
+                            garage.CheckOut(regNr);
+                            break;
+                        case "n":
+                            return;
+                    }
+                }
+            }
+            catch (EVehicleNotFound e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
 
-                    break;
+        private static string ConstrainInput(string question, string[] allowedValues)
+        {
+            string input;
+            while (true)
+            {
+                Console.Write(question);
+                input = Console.ReadLine();
+                foreach(string s in allowedValues)
+                {
+                    if (input == s)
+                        return input;
                 }
             }
         }

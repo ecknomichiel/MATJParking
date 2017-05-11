a2ca07ec097182c284365e1117b51e6c590035c7
AhAh10 <ahmadi2001@yahoo.com>
2017-05-11 14:57:15 +0200

    Created main menu

--- MATJParking/Program.cs
+++ MATJParking/Program.cs
@@ -10,7 +10,24 @@ namespace MATJParking
     {
         static void Main(string[] args)
         {
+            //Main menu
+            bool runProgram = true;
+            while (runProgram == true)
+
+           {
+              Console.Clear();  // Clears console
+    Console.WriteLine("Welcome to the MATJParking! Please navigate through the menu: \n1.CheckIn \n2.CheckOut \n3.Search for your Vehicle \n4.Search for multiple Vehicles \n5.See all Vehicles at Garage \n6.Exit");
+      		  Console.WriteLine("Please enter your selection: ");
+
+                    switch (Console.ReadLine())  //switch statement 
+                    {
+                     case "1":
+
+                     break;
+                      }
+
+                 }
+            }
 
         }
     }
-}
